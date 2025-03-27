ce939f57912c95eb714873530c74a5f4bdd41c61
Yi Rui Goh <yirui94@gmail.com>
2025-03-28 05:37:37 +0800

    feat: add redux store for handling notes

---
+++ redux/notesSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Category } from '@/constants/Constants';
+import { RootState } from '@/redux/store';
+
+// Define a type for the slice state
+// Latest note always at the end of the array
+export interface NotesState {
+    [Category.Work]: string[],
+    [Category.Life]: string[],
+    [Category.Health]: string[],
+}
+  
+  // Define the initial state using that type
+const initialState: NotesState = {
+    // [Category.Work]: ['Overview of basic computer networking knowledge', 'How to calculate float multiplication and division in JavaScript?'],
+    // [Category.Life]: ['Pan-fried chicken breast with vegetable salad'],
+    // [Category.Health]: ['Maintain sufficient daily water intake'],
+    [Category.Work]: [],
+    [Category.Life]: [],
+    [Category.Health]: [],
+}
+
+export const notesSlice = createSlice({
+  name: 'notes',
+  initialState,
+  // Redux toolkit detects "mutation" here, though not reassignment
+  reducers: {
+    addNote: (state, action: PayloadAction<{category: typeof Category[keyof typeof Category] , note: string}>) => {
+        state[action.payload.category].push(action.payload.note)
+    },
+    deleteAll: (state) => {
+        return initialState
+    },
+  },
+});
+
+export const { addNote, deleteAll } = notesSlice.actions;
+
+export const selectAllNotes = (state: RootState) => state.notes;
+export const selectWorkNotes = (state: RootState) => state.notes[Category.Work];
+export const selectLifeNotes = (state: RootState) => state.notes[Category.Life];
+export const selectHealthNotes = (state: RootState) => state.notes[Category.Health];
+
+export default notesSlice.reducer;
---
+++ redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import notesReducer from '@/redux/notesSlice';
+
+export const store = configureStore({
+  reducer: {
+    notes: notesReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type AppStore = typeof store
